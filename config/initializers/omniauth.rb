ba9cce39b0f5596d5d18f9e22e8b21b16763d46c
Ramin <rsedighi@gmail.com>
2014-06-03 14:59:39 -0700

    minor fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,13 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
       :name => "google",
-      :scope => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+      :scope => 'https://www.googleapis.com/auth/userinfo.email',
       :access_type => 'offline',
       :prompt => "select_account consent",
       :image_aspect_ratio => "square",
       :image_size => 50
     }
 end
-
-
-
